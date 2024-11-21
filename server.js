29db4224d6435b7d2b3144e62abd231cf8c93ed2
anuzsubedi <anuzsubedi@hotmail.com>
2024-11-21 01:33:09 -0500

    Add CORS middleware to server and update package.json and package-lock.json

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const initializeTables = require('./config/initTables');
 
 require('dotenv').config();
+initializeTables();
 
 const app = express();
+
+// Middleware
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// Configure and Restrict CORS
+app.use(
+    cors({
+        origin: "http://localhost:5173", // Replace with your frontend's URL
+        methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+        allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+        credentials: true, // Allow cookies and credentials
+    })
+);
+
+// Routes
 app.use('/auth', authRoutes);
 
+// Start the Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
